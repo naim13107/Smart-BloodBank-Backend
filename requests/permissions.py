4a7be8abe3f4c5d02170a1069129873cc3685f05
MD.Naim-UL-Haque <naimulh644@gmail.com>
2026-02-26 01:10:23 +0600

    new fix added on request permission

--- requests/permissions.py
+++ requests/permissions.py
@@ -5,12 +5,11 @@ class IsRecipientOrAdmin(permissions.BasePermission):
     Allows anyone to view, but only the recipient or admin to edit/delete.
     """
     def has_object_permission(self, request, view, obj):
-       
         if request.method in permissions.SAFE_METHODS:
             return True
-        if view.action == 'accept':
-            return request.user.is_authenticated
-        if request.user.is_staff:
+        
+        if request.user and request.user.is_staff:
             return True
             
+
         return obj.recipient == request.user
